7b08a9d5c90d572511e87e01f979499f259cd14e
Samandar <shavkatovsamandar54@gmail.com>
2026-03-11 14:45:58 +0500

    Добавить базовую структуру проекта с настройками, представлениями и шаблонами для онлайн-магазина

--- Backend/online_shop/views.py
+++ Backend/online_shop/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+﻿from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    text = "Hello world"
+    content = {"text": text}
+    return render(request, "home.html", content)
+
+
+def about(request):
+    text = "About us"
+    content = {"text": text}
+    return render(request, "about.html", content)
